cca5dc503b66f446082b66e11316778e4a12ef6d
Alejandro Baltra <abaltra@gmail.com>
2020-05-28 01:45:18 -0400

    Added terraform

---
+++ terraform/s3.tf
@@ -0,0 +1,20 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = "s3-presigned-upload-example"
+  acl    = "public-read"
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
